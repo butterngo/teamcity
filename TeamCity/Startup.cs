d9b4501354ae75ea3f471467d3b3a7477b0234b2
VuNgo <vu.ngo@tekcent.com>
2017-07-17 23:17:03 +0700

    Init Code

---
+++ TeamCity/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+
+[assembly: OwinStartupAttribute(typeof(TeamCity.Startup))]
+namespace TeamCity
+{
+    
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+           
+        }
+    }
+}
---
+++ TeamCity/gulp.cmd
@@ -0,0 +1,3 @@
+@echo off
+..\packages\Ncapsulate.Node.0.10.26\nodejs\node ..\packages\Ncapsulate.Gulp.3.8.10.1\nodejs\node_modules\gulp\bin\gulp --no-color %*
+@echo on
